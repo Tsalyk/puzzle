1824abb977d07474f50af619e64c2f543f6066fb
Tsalyk <zalukm1@gmail.com>
2021-02-05 20:03:41 +0200

    create functions and add requirements

---
+++ puzzle.py
@@ -0,0 +1,13 @@
+def check_rows(board: list) -> bool:
+    pass
+
+
+def check_columns(board: list) -> bool:
+    pass
+
+
+def check_bloc(board: list) -> bool:
+    pass
+
+def validate_board(board: list) -> bool:
+    pass
